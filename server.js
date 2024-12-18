93d6b289b122ff5cd3b19072598d5f102b88b7e8
Amal ks <amalksaml777@gmail.com>
2024-12-18 12:24:01 +0530

    menu_app backend

---
+++ server.js
@@ -0,0 +1,49 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import pg from "pg";
+import env from "dotenv";
+
+
+const app = express();
+const port = 5000;
+env.config();
+
+const db = new pg.Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+});
+db.connect();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.get('/menus',async (req, res) => {
+    try {
+        const result = await db.query('SELECT * FROM menus');
+        res.json(result.rows);
+    } catch (error) {
+        console.log("error");
+        res.status(400).json({error:'server error'});
+    }
+});
+
+app.get('/menu/:id/items', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const result = await db.query('SELECT * FROM items WHERE menu_id = $1', [id]);
+        res.json(result.rows);
+        console.log(result.rows);
+    } catch (error) {
+        console.log("error fetch data");
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Listening to port http://localhost:${port}`);
+})
